8f249321a4b3b2ff2ea5b4f72e87ca45f3e69266
Damien Nivault <damien.nivault@supinternet.fr>
2018-01-23 14:07:27 +0100

    Initial Commit

---
+++ Exercice_6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exercice_6
+//
+//  Created by Damien Nivault on 23/01/2018.
+//  Copyright © 2018 Damien Nivault. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
